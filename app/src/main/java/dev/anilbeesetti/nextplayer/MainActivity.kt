4cbfa2c968fe649ffd085f44c75bb7f9ebada910
Anil Kumar Beesetti <anilbeesetti10@gmail.com>
2024-02-19 09:10:34 +0530

    add edge-to-edge screen

--- app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
+++ app/src/main/java/dev/anilbeesetti/nextplayer/MainActivity.kt
@@ -1,10 +1,13 @@
 package dev.anilbeesetti.nextplayer
 
 import android.Manifest
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,6 +25,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.compose.NavHost
@@ -74,7 +78,10 @@ class MainActivity : ComponentActivity() {
         }
 
         installSplashScreen()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         setContent {
             NextPlayerTheme(
@@ -87,16 +94,10 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.surface
                 ) {
                     val storagePermissionState = rememberPermissionState(permission = storagePermission)
-                    val lifecycleOwner = LocalLifecycleOwner.current
 
-                    DisposableEffect(key1 = lifecycleOwner) {
-                        val observer = LifecycleEventObserver { _, event ->
-                            if (event == Lifecycle.Event.ON_START) {
-                                storagePermissionState.launchPermissionRequest()
-                            }
-                        }
-                        lifecycleOwner.lifecycle.addObserver(observer)
-                        onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
+                    LifecycleStartEffect(Unit) {
+                        storagePermissionState.launchPermissionRequest()
+                        onStopOrDispose {  }
                     }
 
                     LaunchedEffect(key1 = storagePermissionState.status.isGranted) {
